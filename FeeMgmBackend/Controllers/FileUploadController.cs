a29035860c6e606dd8b55eb2b7c7cc5efc87d22e
Md Fahimul Karim <f.nahian1997@gmail.com>
2023-12-04 19:19:50 +0600

    external File upload completed

---
+++ FeeMgmBackend/Controllers/FileUploadController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FeeMgmBackend.Services;
+using Microsoft.EntityFrameworkCore;
+using FeeMgmBackend.Dtos;
+using FeeMgmBackend.Entity;
+
+namespace FeeMgmBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilesController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public FilesController(DatabaseContext dbContext)
+        {
+            _context = dbContext;
+        }
+        [HttpPost("Fileupload")]
+        public async Task< IActionResult> AnalyzeExcelFile(IFormFile file)
+        {
+            var streamFile = file.OpenReadStream();
+            var excelService = new ExcelService();
+            var records = excelService.ParseExcel(streamFile);
+            var rows = records.Count;
+            var col = rows>0? records[0].Count: 0;
+            for(int i = 1; i<rows; i++)
+            {
+                string userName="",lawName="";
+                decimal amount=0;
+                DateTime date;
+
+                for(int j = 0; j<col; j++)
+                {
+                    switch (j)
+                    {
+                        case 0:
+                            userName = records[i][j].ToString();
+                            break;
+                        case 1:
+                            try
+                            {
+                                if (records[i][j].ToString().Length > 0)
+                                    amount = decimal.Parse(records[i][j].ToString());
+                                else amount = 0;
+                
+                            }
+                            catch (FormatException ex)
+                            {
+                                throw new Exception();
+                            }
+                            break;
+                        case 2:
+                            lawName = records[i][j].ToString();
+                            break;
+                        case 3:
+                            Console.WriteLine("Implement paid - payment status");
+                            break;
+                        case 4:
+                            date = records[i][j].ToString().Length> 0? DateTime.Parse(records[i][j].ToString()) : DateTime.Now;
+                            break;
+                            
+                    }
+                }
+                User user =  await _context.Users.FirstOrDefaultAsync(u => u.Name == userName);
+
+                if(user == null)
+                {
+                    user = new User();
+                    user.Name = userName;
+                    await _context.Users.AddAsync(user);
+                    await _context.SaveChangesAsync();
+                }else if (user.IsDeactivated)
+                {
+                    user.IsDeactivated = false;
+                    _context.Users.Update(user);
+                }
+                Law law = await _context.Laws.FirstOrDefaultAsync(l => l.Name == lawName);
+                if(law==null)
+                {
+                    law = new Law();
+                    law.Name = lawName;
+                    law.Amount = amount;
+                    law.IsDeleted = false;
+                    await _context.Laws.AddAsync(law);
+                    await _context.SaveChangesAsync();
+                }else if(law.IsDeleted)
+                {
+                    law.IsDeleted = false;
+                    _context.Laws.Update(law);
+                }
+                Fine fine = new Fine();
+                var existingLaw = await _context.Laws.FirstOrDefaultAsync(l => l.Name == lawName);
+                var existingUser = await _context.Users.FirstOrDefaultAsync(u => u.Name == userName);
+                fine.LawId = existingLaw.Id;
+                fine.UserId = existingUser.Id;
+             //   fine.UpdatedAt = date??DateTime.Now;
+                await _context.Fines.AddAsync(fine);
+                await _context.SaveChangesAsync();
+            }
+            return Ok(records);
+        }
+
+        
+    }
+}
---
+++ FeeMgmBackend/Services/ExcelService.cs
@@ -0,0 +1,45 @@
+﻿using OfficeOpenXml;
+
+namespace FeeMgmBackend.Services
+{
+    public class ExcelService
+    {
+        public ExcelService() { }
+        public List<List<object>> ParseExcel(Stream file)
+        {
+            ExcelPackage package = new ExcelPackage(file);
+            ExcelWorksheet worksheet = package.Workbook.Worksheets.FirstOrDefault();
+            List<List<object>> records = new List<List<object>>();
+            List<int> skippedIndex = new List<int>();
+            int rows = worksheet?.Dimension.Rows ?? 0;
+            if (rows == 0) return records;
+            int columns = worksheet?.Dimension.Columns ?? 0;
+            if (columns == 0) return records;
+
+
+            for (int i = 1; i <= rows; i++)
+            {
+                var singleRow = GetRow(worksheet, i, columns, skippedIndex);
+                records.Add(singleRow);
+            }
+            return records;
+        }
+            
+
+
+    private List<object> GetRow(ExcelWorksheet worksheet, int row, int columns, List<int> skippedIndex)
+    {
+        List<object> rowData = new List<object>();
+        for (int j = 1; j <= columns; j++)
+        {
+            if (row == 1 && (worksheet.Cells[row, j] == null || string.IsNullOrEmpty(worksheet.Cells[row, j].Text))) skippedIndex.Add(j);
+
+            if (skippedIndex.Contains(j)) continue;
+
+            object content = worksheet.Cells[row, j].Text;
+            rowData.Add(content);
+        }
+        return rowData;
+    }
+}
+}
